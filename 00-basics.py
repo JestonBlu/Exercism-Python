30a348c6e577c7019d5ef7ee8a0b9ea1cba9b57a
jestonblu <jestonblu@gmail.com>
2017-07-18 22:16:29 -0500

    added file

---
+++ 00-basics.py
@@ -0,0 +1,14 @@
+'''
+Basic Python Commands
+'''
+
+## This is a list
+x = [1, 2, 3, 4]
+
+## This is a tuple
+x = 1, 2, 3, 4
+
+## This is a dictionary
+x = {'label1': 1, 'label2': 2}
+
+## This is a loop
