f0bbbb6424c60153761a0dc6b0006d6015de219c
jestonblu <jestonblu@gmail.com>
2017-07-20 21:17:19 -0500

    updated basics

--- 00-basics.py
+++ 00-basics.py
@@ -4,11 +4,14 @@ Basic Python Commands
 
 ## This is a list
 x = [1, 2, 3, 4]
+print(x)
 
 ## This is a tuple
 x = 1, 2, 3, 4
+print(x)
 
 ## This is a dictionary
 x = {'label1': 1, 'label2': 2}
+print(x)
 
 ## This is a loop
